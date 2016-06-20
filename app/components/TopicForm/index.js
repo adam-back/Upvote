8100d78e713273e48d7b348403c7d2386de6a0f9
Adam Back <adb33@email.arizona.edu>
2016-06-19 17:05:08 -0700

    (feat) Highjack form's submit.

--- app/components/TopicForm/index.js
+++ app/components/TopicForm/index.js
@@ -7,14 +7,27 @@
 import React from 'react';
 
 import styles from './styles.css';
+var ENTER_KEY = 13;
 
 class TopicForm extends React.Component {
+  handleSubmit( e ) {
+    e.preventDefault();
+    // var author = this.state.author.trim();
+    // var text = this.state.text.trim();
+    console.log( 'here')
+    if (!text || !author) {
+      return;
+    }
+    // TODO: send request to the server
+    // this.setState({author: '', text: ''});
+  }
+
   render() {
     return (
       <div className={styles.topicForm}>
-        <form name="topicForm">
+        <form name="topicForm" onSubmit={this.handleSubmit}>
           <p>
-            <input type="text" placeholder="What do you want to learn?"/>
+            <input type="text" placeholder="What do you want to learn?" autoFocus={true}/>
             <button type="submit">Submit</button>
           </p>
         </form>
